cc679b9951378ff1da132d360b5b680d38ea82ce
yujin9747 <jyj9747@naver.com>
2022-07-04 22:08:56 +0900

    Day1_A007

---
+++ 0704_Day1/A007_장유진_22000630.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, ascending=1, descending=1;
+    for(int i=0; i<8; i++) {
+        cin >> a;
+        if(a != i+1) ascending = 0; // ascending이 아닌 경우
+        if(a != 8-i) descending = 0; // descending이 아닌 경우
+    }
+    if(ascending == 0 && descending == 0) cout << "mixed";
+    else if(ascending == 1) cout << "ascending";
+    else cout << "descending";
+    return 0;
+}
